30cdd1784bb6d326ace2e0cfaf777912bc1c13f9
Ryan Hausmann <ryanhausmann3@gmail.com>
2025-07-06 13:41:47 -0400

    youtube-song-info: extract video title and description from given video

--- youtube-song-info/main.py
+++ youtube-song-info/main.py
@@ -1,6 +1,7 @@
 #! /bin/python3
 import os
 import sys
+import json
 from dotenv import load_dotenv
 from pyyoutube import Api 
 
@@ -16,7 +17,15 @@ def main():
     api = Api(api_key=YOUTUBE_API_KEY)
 
     video = api.get_video_by_id(video_id=video_id)
-    print(video.items[0].to_json())
+    video_json = video.items[0].to_json()
+
+    video = json.loads(video_json)
+
+    video_title = video["snippet"]["title"]
+    video_desc = video["snippet"]["description"]
+
+    print(f"Title: \"{video_title}\"")
+    print(f"Description:\n{video_desc}")
 
 if __name__ == "__main__":
     main()
