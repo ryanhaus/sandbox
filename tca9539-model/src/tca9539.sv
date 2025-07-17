45385a3810ab813befe1e97d513d15b29c82b3a7
Ryan Hausmann <ryanhausmann3@gmail.com>
2025-07-17 11:46:36 -0400

    tca9539-model: Add IC pins to top-level module

--- tca9539-model/src/tca9539.sv
+++ tca9539-model/src/tca9539.sv
@@ -1,3 +1,16 @@
-module tca9539();
+module tca9539(
+	input reset_n, // Reset (active low)
+	output int_n,  // Interrupt (active low)
+
+	// I2C interface
+	input scl,
+	inout sda,
+
+	// I2C address selection
+	input a0, a1
+
+	// GPIOs (note: only [7:0] and [17:10] are valid, [9:8] are not driven)
+	inout [17:0] port
+);
 
 endmodule
