a88dffd598ab9dbce2b970c3eb77269ec9dd3791
Ryan Hausmann <ryanhausmann3@gmail.com>
2025-07-23 15:49:29 -0400

    tca9539-model: Modify I2C slave & register interface to match that of the TCA9539

--- tca9539-model/src/tca9539.sv
+++ tca9539-model/src/tca9539.sv
@@ -18,35 +18,6 @@ module tca9539(
 
     logic rst = ~reset_n;
 
-    // I2C slave
-    i2cSlave_tca9539 i2c_slave (
-        .clk(clk),
-        .rst(rst),
-        .sda(sda),
-        .scl(scl),
-        .deviceAddress({ 5'b11101, addr_sel }), // From Table 2
-        .myReg0(),
-        .myReg1(),
-        .myReg2(),
-        .myReg3(),
-        .myReg4(),
-        .myReg5(),
-        .myReg6(),
-        .myReg7()
-    );
-
-    // TCA9539 commands, see table 3 in datasheet
-    typedef enum bit [2:0] {
-        INPUT_PORT_0              = 3'b000,
-        INPUT_PORT_1              = 3'b001,
-        OUTPUT_PORT_0             = 3'b010,
-        OUTPUT_PORT_1             = 3'b011,
-        POLARITY_INVERSION_PORT_0 = 3'b100,
-        POLARITY_INVERSION_PORT_1 = 3'b101,
-        CONFIGURATION_PORT_0      = 3'b110,
-        CONFIGURATION_PORT_1      = 3'b111
-    } command;
-
     // TCA9539 internal registers, table 3
     logic [7:0] input_port_0,
                 input_port_1,
@@ -62,6 +33,23 @@ module tca9539(
                  reg_output             = { output_port_1,             output_port_0             },
                  reg_polarity_inversion = { polarity_inversion_port_1, polarity_inversion_port_0 },
                  reg_configuration      = { configuration_port_1,      configuration_port_0      };
+
+    // I2C slave
+    i2cSlave_tca9539 i2c_slave (
+        .clk(clk),
+        .rst(rst),
+        .sda(sda),
+        .scl(scl),
+        .deviceAddress({ 5'b11101, addr_sel }), // From Table 2
+        .input_port_0(input_port_0),
+        .input_port_1(input_port_1),
+        .output_port_0(output_port_0),
+        .output_port_1(output_port_1),
+        .polarity_inversion_port_0(polarity_inversion_port_0),
+        .polarity_inversion_port_1(polarity_inversion_port_1),
+        .configuration_port_0(configuration_port_0),
+        .configuration_port_1(configuration_port_1)
+    );
 
     // 'reg_input'
     always_comb reg_input = port ^ reg_polarity_inversion;
