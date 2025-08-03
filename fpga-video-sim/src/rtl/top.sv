edff4c2ba850f1598949296f64dacd799544fdd4
Ryan Hausmann <ryanhausmann3@gmail.com>
2025-08-03 16:09:48 -0400

    fpga-video-sim: Fix systemverilog syntax errors

--- fpga-video-sim/src/rtl/top.sv
+++ fpga-video-sim/src/rtl/top.sv
@@ -5,16 +5,14 @@ module top(
     output dotclk,
     output hsync,
     output vsync,
-    output [7:0] red,
-    output [7:0] green,
-    output [7:0] blue
+    output logic [7:0] red,
+    output logic [7:0] green,
+    output logic [7:0] blue
 );
 
     // generate timing signals (default timing parameters for ILI9341)
-    localparam CTR_H_BITS = timing_gen_inst.HORIZONTAL_CTR_BITS;
-    localparam CTR_V_BITS = timing_gen_inst.VERTICAL_CTR_BITS;
-
-    logic [CTR_H_BITS-1 : 0] display_pos_h, display_pos_v;
+    logic [timing_gen_inst.HORIZONTAL_CTR_BITS-1 : 0] display_pos_h;
+    logic [timing_gen_inst.VERTICAL_CTR_BITS-1 : 0] display_pos_v;
     logic in_display_region;
 
     video_timing_gen timing_gen_inst (
@@ -29,8 +27,10 @@ module top(
     );
 
     // generate video
-    red = display_pos_h[7:0];
-    green = display_pos_v[7:0];
-    blue = 8'h80;
+    always_comb begin
+        red = display_pos_h[7:0];
+        green = display_pos_v[7:0];
+        blue = 8'h80;
+    end
 
 endmodule
