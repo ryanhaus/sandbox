e67214fae487ebb37e00c5757dc281fcd5f0b125
Ryan Hausmann <ryanhausmann3@gmail.com>
2025-08-03 17:54:17 -0400

    fpga-video-sim: Begin writing tests using marlin

---
+++ fpga-video-sim/tests/video_timing_gen.rs
@@ -0,0 +1,38 @@
+use marlin::{
+    verilator::{VerilatedModelConfig, VerilatorRuntime, VerilatorRuntimeOptions},
+    verilog::prelude::*,
+};
+use snafu::Whatever;
+
+#[verilog(src="src/rtl/video_timing_gen.sv", name="video_timing_gen")]
+struct VideoTimingGen;
+
+const MAX_SIM_TIME: u32 = 1_000_000;
+
+#[test]
+fn video_timing_gen() -> Result<(), Whatever> {
+    let runtime = VerilatorRuntime::new(
+        "artifacts".into(),
+        &["src/rtl/top.sv".as_ref()],
+        &["src/rtl".into()],
+        [],
+        VerilatorRuntimeOptions::default_logging(),
+    )?;
+
+    let mut main = runtime.create_model::<VideoTimingGen>(
+        &VerilatedModelConfig {
+            enable_tracing: true,
+            ..Default::default()
+        }
+    )?;
+
+    let mut vcd = main.open_vcd("video_timing_gen.vcd");
+
+    let mut sim_time = 0;
+
+    while sim_time < MAX_SIM_TIME {
+        sim_time += 1;
+    }
+
+    Ok(())
+}
