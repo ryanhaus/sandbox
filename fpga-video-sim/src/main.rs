cea2d6e0fe4717ff4dbee33bd050888ff804ae18
Ryan Hausmann <ryanhausmann3@gmail.com>
2025-08-03 16:11:31 -0400

    fpga-video-sim: Start updating simulation program to use marlin for hardware simulation

--- fpga-video-sim/src/main.rs
+++ fpga-video-sim/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use marlin::{
+    verilator::{VerilatorRuntime, VerilatorRuntimeOptions},
+    verilog::prelude::*,
+};
+
+use snafu::Whatever;
+
+#[verilog(src="src/rtl/top.sv", name="top")]
+struct Top;
+
+#[snafu::report]
+fn main() -> Result<(), Whatever> {
+    let runtime = VerilatorRuntime::new(
+        "artifacts".into(),
+        &["src/rtl/top.sv".as_ref()],
+        &["src/rtl".into()],
+        [],
+        VerilatorRuntimeOptions::default_logging(),
+    )?;
+
+    let mut top = runtime.create_model_simple::<Top>()?;
+
+    Ok(())
 }
