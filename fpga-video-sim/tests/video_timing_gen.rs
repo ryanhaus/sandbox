0dd3cc55fb3fa65d52a342b2b89d7c7c61cc301c
Ryan Hausmann <ryanhausmann3@gmail.com>
2025-08-04 20:16:16 -0400

    fpga-video-sim: Add test for HSYNC period

--- fpga-video-sim/tests/video_timing_gen.rs
+++ fpga-video-sim/tests/video_timing_gen.rs
@@ -8,6 +8,18 @@ use snafu::Whatever;
 #[verilog(src="src/rtl/video_timing_gen.sv", name="video_timing_gen")]
 struct VideoTimingGen;
 
+const DOTCLK_DIV: usize = 10;
+const PERIOD_HSYNC: usize = 10;
+const PERIOD_HBP: usize = 20;
+const PERIOD_HACTIVE: usize = 240;
+const PERIOD_HFP: usize = 10;
+const PERIOD_HORIZONTAL: usize = PERIOD_HSYNC + PERIOD_HBP + PERIOD_HACTIVE + PERIOD_HFP;
+const PERIOD_VSYNC: usize = 2;
+const PERIOD_VBP: usize = 2;
+const PERIOD_VACTIVE: usize = 320;
+const PERIOD_VFP: usize = 4;
+const PERIOD_VERTICAL: usize = PERIOD_VSYNC + PERIOD_VBP + PERIOD_VACTIVE + PERIOD_VFP;
+
 macro_rules! setup_runtime {
     () => {{
         VerilatorRuntime::new(
@@ -34,6 +46,16 @@ macro_rules! reset_top_module {
     };
 }
 
+macro_rules! clock_cycle {
+    ($top:ident) => {
+        $top.clk = 0;
+        $top.eval();
+
+        $top.clk = 1;
+        $top.eval();
+    };
+}
+
 #[test]
 fn check_dotclk_division() -> Result<(), Whatever> {
     let runtime = setup_runtime!();
@@ -45,12 +67,38 @@ fn check_dotclk_division() -> Result<(), Whatever> {
             let expected_dotclk = if i >= 5 { 0 } else { 1 };
             if top.dotclk != expected_dotclk { panic!(); }
             
-            top.clk = 0;
-            top.eval();
-
-            top.clk = 1;
-            top.eval();
+            clock_cycle!(top);
         }
+    }
+
+    Ok(())
+}
+
+#[test]
+fn check_hsync_period() -> Result<(), Whatever> {
+    let runtime = setup_runtime!();
+    let mut top = runtime.create_model_simple::<VideoTimingGen>()?;
+    reset_top_module!(top);
+
+    for cycle in 0..5 {
+        let mut clks_0 = 0;
+        let mut clks_1 = 0;
+
+        while top.hsync == 0 {
+            clks_0 += 1;
+            clock_cycle!(top);
+        }
+        
+        while top.hsync == 1 {
+            clks_1 += 1;
+            clock_cycle!(top);
+        }
+
+        clks_0 /= DOTCLK_DIV;
+        clks_1 /= DOTCLK_DIV;
+
+        if clks_0 != PERIOD_HSYNC { panic!(); }
+        if clks_0 + clks_1 != PERIOD_HORIZONTAL { panic!(); }
     }
 
     Ok(())
