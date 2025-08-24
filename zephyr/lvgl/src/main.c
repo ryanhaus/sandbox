9226b82b8d1f6c629667f2a1ccef0b3dcb6e44f8
Ryan Hausmann <ryanhausmann3@gmail.com>
2025-08-24 19:11:19 -0400

    zephyr/lvgl: Add simple label to app

--- zephyr/lvgl/src/main.c
+++ zephyr/lvgl/src/main.c
@@ -5,14 +5,36 @@
 
 #include <lvgl.h>
 
+void setup_gui(void)
+{
+    // set background color
+    lv_obj_set_style_bg_color(
+        lv_screen_active(),
+        lv_color_hex(0xFF0000),
+        LV_PART_MAIN
+    );
+
+    // create label
+    lv_obj_t* label = lv_label_create(lv_screen_active());
+    lv_label_set_text(label, "Hello");
+
+    lv_obj_set_style_text_color(
+        lv_screen_active(),
+        lv_color_hex(0xFFFFFF),
+        LV_PART_MAIN
+    );
+
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+}
+
 int main(void)
 {
-    const struct device *display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+    const struct device* display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
 
     if (!device_is_ready(display_dev))
         return -ENODEV;
 
-    // initialize_gui();
+    setup_gui();
 
     lv_timer_handler();
     display_blanking_off(display_dev);
