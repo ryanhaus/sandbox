732c243e994bc5b6bf6db075c18522aa0a63073d
Ryan Hausmann <ryanhausmann3@gmail.com>
2025-08-24 19:26:33 -0400

    zephyr/threads: Run something on the main thread, as well as a dynamically spawned thread

--- zephyr/threads/src/main.c
+++ zephyr/threads/src/main.c
@@ -12,19 +12,31 @@ void printer_thread(const char* message, uint32_t sleep_time)
     }
 }
 
-K_THREAD_DEFINE(
-    printer_thread0,
-    1024,
-    printer_thread,
-    "Hello thread 0\n", 500, NULL,
-    7, 0, 0
-);
+K_THREAD_STACK_DEFINE(thread_stack_area, 1024);
+
+int main(void)
+{
+    struct k_thread thread_data;
+
+    k_thread_create(
+        &thread_data,
+        thread_stack_area,
+        K_THREAD_STACK_SIZEOF(thread_stack_area),
+        printer_thread,
+        "Hello dynamic thread\n", 750, NULL,
+        7, 0, K_NO_WAIT
+    );
+    
+    printer_thread("Hello main thread\n", 1000);
+
+    return 0;
+}
 
 K_THREAD_DEFINE(
     printer_thread1,
     1024,
     printer_thread,
-    "Hello thread 1\n", 300, NULL,
+    "Hello static thread 1\n", 500, NULL,
     7, 0, 0
 );
 
@@ -32,6 +44,6 @@ K_THREAD_DEFINE(
     printer_thread2,
     1024,
     printer_thread,
-    "Hello thread 2\n", 500, NULL,
+    "Hello static thread 2\n", 300, NULL,
     7, 0, 0
 );
